d3db8dfa3abc38becfd54100705aa1ed2df7a0a4
Aidan <aidan.s.ranney@gmail.com>
2019-07-11 15:07:15 -0700

    Plugins processing images and markdown. Node APIs in use.

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,7 +24,7 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <div>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
@@ -36,12 +36,10 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © Aidan Ranney {new Date().getFullYear()}
         </footer>
       </div>
-    </>
+    </div>
   )
 }
 
