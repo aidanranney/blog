88eb0bb9c1a529f429ddfe41139af1348444d455
Aidan Ranney <aidan.s.ranney@gmail.com>
2021-01-30 16:58:56 -0800

    Add netlify cms dependencies and plugin to config
    This updates remaining site dependencies along with the addition,
    via `yarn`.

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
     },
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
